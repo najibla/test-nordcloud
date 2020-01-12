2592940b10e1ea29feaf27d400afbee8a9692214
Mohamed Najib Laaziz <m.najib.laaziz@gmail.com>
2020-01-13 01:35:52 +0200

    Updates

---
+++ precache-manifest.6ecc747ec3a2df722406b1c62b580f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14469c289d6bdf0086ef49b7a710428a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d9cd286854fbc9ba70b",
+    "url": "/static/css/main.072720d4.chunk.css"
+  },
+  {
+    "revision": "16d801db4bda11dc89da",
+    "url": "/static/js/2.da755f6b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.da755f6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d9cd286854fbc9ba70b",
+    "url": "/static/js/main.a5ac9ea8.chunk.js"
+  },
+  {
+    "revision": "65c8cc008596a5802bcb",
+    "url": "/static/js/runtime-main.5e82db21.js"
+  }
+]);
